f595f6fd37c8b6a2bfc8619f46b0c90d2e6fe058
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-17 00:57:25 +0100

    ds

--- vue/Page_Acceuil.vue.php
+++ vue/Page_Acceuil.vue.php
@@ -13,7 +13,7 @@
       </div>
       <div class="rubrique">
         <a href="../vue/Galerie.vue.php">Galerie</a>
-        <a href="../vue/Travail.vue.php">Notre travail</a>
+        <a href="../vue/Travail.vue.php">Mon travail</a>
         <a href="../vue/Avis.vue.php">Avis</a>
         <a href="../vue/Contact.vue.php">Contactez-Nous</a>
       </div>
