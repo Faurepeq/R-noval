58a9ee0f4afd97f2de1a4f0113b2168a8ddbc9f9
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-17 21:53:55 +0100

    dqs

--- vue/Avis.vue.php
+++ vue/Avis.vue.php
@@ -40,7 +40,7 @@
         echo"<div class=\"unAvis\">";
           echo"<h4>Un utilisateur</h4>";
           echo"<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
-          echo"<p>NB etoiles sur 5</p>";
+          echo"<h5>NB etoiles sur 5</h5>";
         echo"</div>";
       }
      ?>
