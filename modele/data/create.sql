fbce062abc2be66ebc3f37970aab9f8c8f920033
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-18 22:12:03 +0100

    tgr

---
+++ modele/data/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE projet{
+  id Int(6) UNSIGNED AUTO_INCREMENT primary key,
+  nomProjet VARCHAR(30),
+  avant VARCHAR(50) NOT NULL,
+  apres VARCHAR(50) NOT NULL
+  }
+
+CREATE TABLE avis{
+  numero int(6) UNSIGNED AUTO_INCREMENT primary key,
+  nomUtilisateur VARCHAR (20) NOT NULL,
+  texte VARCHAR(1000) NOT NULL,
+  etoiles int(1) UNSIGNED NOT NULL
+  }
