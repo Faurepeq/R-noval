346fc34c905d89ee25102bac75bf502e87672de3
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-18 22:26:23 +0100

    jkh

--- modele/data/create.sql
+++ modele/data/create.sql
@@ -1,13 +1,13 @@
-CREATE TABLE projet{
+CREATE TABLE if not exists projet(
   id Int(6) UNSIGNED AUTO_INCREMENT primary key,
   nomProjet VARCHAR(30),
   avant VARCHAR(50) NOT NULL,
   apres VARCHAR(50) NOT NULL
-  };
+)ENGINE=INNODB;
 
-CREATE TABLE avis{
+CREATE TABLE if not exists avis(
   numero int(6) UNSIGNED AUTO_INCREMENT primary key,
   nomUtilisateur VARCHAR (30) NOT NULL,
   texte text NOT NULL,
   etoiles int(1) UNSIGNED NOT NULL
-  };
+);
