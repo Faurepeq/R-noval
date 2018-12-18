6da8a4ee81f2eb251ad9d72d6a3a7e87f36e4aee
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-18 22:21:11 +0100

    hfgh

--- modele/data/create.sql
+++ modele/data/create.sql
@@ -3,11 +3,11 @@ CREATE TABLE projet{
   nomProjet VARCHAR(30),
   avant VARCHAR(50) NOT NULL,
   apres VARCHAR(50) NOT NULL
-  }
+  };
 
 CREATE TABLE avis{
   numero int(6) UNSIGNED AUTO_INCREMENT primary key,
   nomUtilisateur VARCHAR (30) NOT NULL,
   texte text NOT NULL,
   etoiles int(1) UNSIGNED NOT NULL
-  }
+  };
