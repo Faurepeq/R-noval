4424dcc92de83868b5516c5ad1f25335c9d7bedc
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-19 17:56:00 +0100

    gfg

--- controller/Avis.controller.php
+++ controller/Avis.controller.php
@@ -2,7 +2,7 @@
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 include('../modele/R-novalDAO.modele.php');
-$requete=$bdd->query('select numero from avis where numero=1');
+$requete=$bdd->query('select nomUtilisateur from avis where numero=1');
 var_dump($requete);
 $resulat=$requete->fetch();
 var_dump($resulat);
