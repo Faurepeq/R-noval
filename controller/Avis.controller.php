f93ec60c7b5546048340b887c51a6ac8774de3d8
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-19 17:47:12 +0100

    hgf

--- controller/Avis.controller.php
+++ controller/Avis.controller.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors','on');
+error_reporting(E_ALL);
 include('../modele/R-novalDAO.modele/php');
 $reponse=$bdd->query('select * from avis');
 echo"$reponse";
--- modele/R-novalDAO.class.php
+++ modele/R-novalDAO.class.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors','on');
+error_reporting(E_ALL);
 try
 {
   $bdd = new PDO('mysql:host=localhost;dbname=Renoval;charset=utf8', 'root','bouze123');
