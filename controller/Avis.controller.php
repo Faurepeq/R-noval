550ace2e588a5af1f2818dec3a3a00e19099acfa
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2018-12-19 18:12:14 +0100

    fdfd

--- controller/Avis.controller.php
+++ controller/Avis.controller.php
@@ -2,9 +2,16 @@
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 include('../modele/R-novalDAO.modele.php');
-$requete=$bdd->query('select nomUtilisateur from avis where numero=1');
-var_dump($requete);
-$resulat=$requete->fetch();
-var_dump($resulat);
+$numero=1;
+$maxNum=$bdd->query("select max(numero)from avis");
+while($numero<=$maxNum){
+  $requete=$bdd->query("select numero,nomUtilisateur,texte,etoiles from avis where numero=$numero");
+  $resultat=$requete->fetch()
+  $nom=$resultat['nomUtilisateur'];
+  $texte=$resultat['texte'];
+  $etoile=$resultat['etoiles'];
+  var_dump($nom);
+  $numero++;
+}
 
  ?>
--- modele/data/insert.sql
+++ modele/data/insert.sql
@@ -4,3 +4,6 @@ insert into projet (nomProjet,avant,apres) values('test2','avant2.jpg','apres2.j
 
 
 insert into avis(nomUtilisateur,texte,etoiles) values ('boulier','je suis un boulier très content',4);
+  insert into avis(nomUtilisateur,texte,etoiles) values ('bite','je suis un boulier très content',2);
+    insert into avis(nomUtilisateur,texte,etoiles) values ('chatte','je suis un boulier très content',5);
+      insert into avis(nomUtilisateur,texte,etoiles) values ('vagin','je suis un boulier très content',3);
